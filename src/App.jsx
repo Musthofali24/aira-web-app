33cf1c3d99bba2e83b50360d8cfec2988a7902f1
Musthofali24 <almus2610@gmail.com>
2025-08-01 11:08:47 +0700

    feat(Notification): Change Toaster to Modal Notification

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,20 @@ import { Toaster } from "react-hot-toast";
 function App({ children }) {
   return (
     <div className="bg-gradient-to-b bg-[#45cad7] text-white min-h-screen py-5">
+      {/* Toast Notifications */}
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          duration: 4000,
+          error: {
+            style: {
+              background: "#FF4B4B",
+              color: "white",
+            },
+          },
+        }}
+      />
+
       {/* Navbar */}
       <Navbar />
 
